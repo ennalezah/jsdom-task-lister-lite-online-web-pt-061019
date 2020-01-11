d882b9114e8ca1b77f55d94cc75f8b0bec9b1b2f
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-01-11 00:51:27 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ document.addEventListener("DOMContentLoaded", () => {
   const inputField = document.getElementById("new-task-description")
   const taskList = document.getElementById("tasks")
 
-  function addTodo(event) {
+  function myTodos(event) {
     let newTask = document.createElement("li")
-    newTask.innerText = inputField.value
+    newTask.innerHTML = `${inputField.value} <button class='delete'>x</button>`
     taskList.appendChild(newTask)
     event.preventDefault()
     inputField.value = ''
+
+    document.querySelector(".delete").addEventListener("click", function(e) {
+      newTask.remove()
+    })
   }
 
-  form.addEventListener("submit", addTodo)
+  form.addEventListener("submit", myTodos)  
 
+  
 });
